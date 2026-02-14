6d313ffd2cbae414db46772571ec9c726f427143
Hayden A. M. <haydenamatthews@gmail.com>
2026-02-14 00:44:30 -0700

    Fixed parallax effect.

--- Team4/Assets/Scripts/ParallaxEffect.cs
+++ Team4/Assets/Scripts/ParallaxEffect.cs
@@ -7,6 +7,8 @@ public class ParallaxEffect : MonoBehaviour
     private float _startingPos; //This is starting position of the sprites.
     private float _lengthOfSprite;    //This is the length of the sprites.
     public float AmountOfParallax;  //This is amount of parallax scroll. 
+    //Higher smoothingFactor means slower
+    public float smoothingFactor = 3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +20,7 @@ public class ParallaxEffect : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 Position = MainCamera.transform.position;
         float Temp = Position.x * (1 - AmountOfParallax);
@@ -26,7 +28,7 @@ public class ParallaxEffect : MonoBehaviour
 
         Vector3 NewPosition = new Vector3(_startingPos + Distance, transform.position.y, transform.position.z);
 
-        transform.position = NewPosition;
+        transform.position = Vector2.Lerp(transform.position, NewPosition, smoothingFactor * Time.deltaTime);
 
         //if (Temp > _startingPos + (_lengthOfSprite / 2))
         //{
