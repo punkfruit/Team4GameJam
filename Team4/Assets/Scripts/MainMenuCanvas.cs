d7114421a2d8d0497c8c9e486b10e454f65d85b8
punkfruit <thecybergrape@gmail.com>
2026-02-15 20:54:42 -0700

    gamepad additions and timer canvas adjustment

--- Team4/Assets/Scripts/MainMenuCanvas.cs
+++ Team4/Assets/Scripts/MainMenuCanvas.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuCanvas : MonoBehaviour
 {
+    public Button startButton;
+
+    private void Start()
+    {
+        startButton.Select();//initally selects the start button so gamepad compatibility is maintained!
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
