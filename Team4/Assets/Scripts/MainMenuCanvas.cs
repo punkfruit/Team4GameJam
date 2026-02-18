ecbe71707eec27e87ee44fbefe71977a339b3dec
punkfruit <thecybergrape@gmail.com>
2026-02-17 17:57:31 -0700

    final build settings

--- Team4/Assets/Scripts/MainMenuCanvas.cs
+++ Team4/Assets/Scripts/MainMenuCanvas.cs
@@ -14,7 +14,7 @@ public class MainMenuCanvas : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void ExitGame()
